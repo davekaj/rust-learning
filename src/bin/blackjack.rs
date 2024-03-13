aefa2c235f779cae3e08db8fc4f1528c511b50c0
David Kajpust <kajpustd@gmail.com>
2024-03-12 22:25:58 -0400

    Bug fix: Add in ability to hold multiple hands after a split

--- src/bin/blackjack.rs
+++ src/bin/blackjack.rs
@@ -38,59 +38,59 @@ fn play_blackjack(balance: &mut u32, bet: &mut u32) {
     println!("Your hand: {:?}, your score: {:?}", player_hands[0], hand_value(&player_hands[0]));
     println!("Dealer shows: {:?}", dealer_hand[0]);
 
-    let mut player_turn = true;
-    while player_turn {
-        println!("Do you want to (h)it, (s)tand, (d)ouble down, or s(p)lit?");
-        let mut action = String::new();
-        io::stdin().read_line(&mut action).expect("Failed to read line");
-        match action.trim() {
-            "h" => {
-                for hand in &mut player_hands {
-                    hand.push(deck.pop().unwrap());
-                }
-                println!("You hit.");
-                print_hands(&player_hands);
-                if any_hand_busted(&player_hands) {
-                    println!("Bust! You lose.");
-                    *balance -= *bet;
-                    return;
-                }
-            }
-            "s" => {
-                println!("You stand.");
-                player_turn = false;
-            }
-            "d" => {
-                if *balance < *bet * 2 {
-                    println!("Insufficient balance to double down.");
-                } else {
-                    for hand in &mut player_hands {
-                        hand.push(deck.pop().unwrap());
+    let mut current_hand_index = 0;
+    while current_hand_index < player_hands.len() {
+        let mut player_turn = true;
+        while player_turn {
+            println!("Hand {}: {:?}, your score: {:?}", current_hand_index + 1, player_hands[current_hand_index], hand_value(&player_hands[current_hand_index]));
+            println!("Do you want to (h)it, (s)tand, (d)ouble down, or (p)lit?");
+            let mut action = String::new();
+            io::stdin().read_line(&mut action).expect("Failed to read line");
+            match action.trim() {
+                "h" => {
+                    player_hands[current_hand_index].push(deck.pop().unwrap());
+                    println!("You hit.");
+                    if hand_value(&player_hands[current_hand_index]) > 21 {
+                        println!("Bust! You lose this hand.");
+                        player_turn = false;
                     }
-                    println!("You double down.");
-                    print_hands(&player_hands);
-                    if any_hand_busted(&player_hands) {
-                        println!("Bust! You lose.");
-                        *balance -= *bet * 2;
-                        return;
-                    }
+                }
+                "s" => {
+                    println!("You stand.");
                     player_turn = false;
-                    *bet *= 2;
+                    current_hand_index += 1;
                 }
-            }
-            "p" => {
-                if player_hands.len() == 1 && can_split(&player_hands[0]) {
-                    let mut new_hand = vec![player_hands[0].pop().unwrap()];
-                    new_hand.push(deck.pop().unwrap());
-                    player_hands.push(new_hand);
-                    player_hands[0].push(deck.pop().unwrap());
-                    println!("You split your hand.");
-                    print_hands(&player_hands);
-                } else {
-                    println!("You cannot split your hand.");
+                "d" => {
+                    if *balance < *bet * 2 {
+                        println!("Insufficient balance to double down.");
+                    } else {
+                        player_hands[current_hand_index].push(deck.pop().unwrap());
+                        println!("You double down.");
+                        if hand_value(&player_hands[current_hand_index]) > 21 {
+                            println!("Bust! You lose this hand.");
+                            *balance -= *bet * 2;
+                            player_turn = false;
+                            current_hand_index += 1;
+                        } else {
+                            player_turn = false;
+                            current_hand_index += 1;
+                            *bet *= 2;
+                        }
+                    }
                 }
+                "p" => {
+                    if player_hands.len() == 1 && can_split(&player_hands[0]) {
+                        let mut new_hand = vec![player_hands[0].pop().unwrap()];
+                        new_hand.push(deck.pop().unwrap());
+                        player_hands.push(new_hand);
+                        player_hands[0].push(deck.pop().unwrap());
+                        println!("You split your hand.");
+                    } else {
+                        println!("You cannot split your hand.");
+                    }
+                }
+                _ => println!("Invalid input. Please enter 'h' to hit, 's' to stand, 'd' to double down, or 'p' to split."),
             }
-            _ => println!("Invalid input. Please enter 'h' to hit, 's' to stand, 'd' to double down, or 'p' to split."),
         }
     }
 
@@ -123,16 +123,6 @@ fn play_blackjack(balance: &mut u32, bet: &mut u32) {
         println!("You lost {} hand(s).", player_loses);
         *balance -= *bet * player_loses as u32;
     }
-}
-
-fn print_hands(player_hands: &[Vec<String>]) {
-    for (i, hand) in player_hands.iter().enumerate() {
-        println!("Your hand {}: {:?}, your score: {:?}", i + 1, hand, hand_value(hand));
-    }
-}
-
-fn any_hand_busted(player_hands: &[Vec<String>]) -> bool {
-    player_hands.iter().any(|hand| hand_value(hand) > 21)
 }
 
 fn can_split(hand: &[String]) -> bool {
